9e688c04adabd0830696441dc1b8e679600f0c68
Antonio-Tone <antoniotone815@gmail.com>
2023-07-07 15:40:16 +0200

    about

---
+++ src/views/AboutView.vue
@@ -0,0 +1,414 @@
+<template>
+  <div class="container main my-5">
+    <div class="about">
+      <div>
+        <div class="quotes p-1 my-4 d-flex flex-row">
+          <h1 class="text-center quote m-0">
+            <span
+              ><Icon
+                icon="ooui:quotes-ltr"
+                color="#646464"
+                class="icon mb-5" /></span
+            >I'M A GREATER BELIEVER IN LUCK, AND I FIND THE HARDER I WORK THE
+            MORE I HAVE OF IT.<span
+              ><Icon
+                icon="ooui:quotes-rtl"
+                color="#646464"
+                class="icon mt-auto mb-5 ms-2"
+            /></span>
+          </h1>
+        </div>
+        <div class="row" id="see">
+          <div class="col text-center my-auto mx-5 styling con">
+            <h2 class="text-center learn mb-4">LEARN MORE ABOUT ME?</h2>
+            <p class="text styling">
+              Hi, I am Antonio Tone and I am 22 years old. I currently reside in
+              Kuil's River where I live with my oldest brother.For as long as I
+              can remember I always wanted to become a programmer and I am
+              thankful to
+              <a
+                class="text-decoration-none"
+                href="https://lifechoices.co.za
+            "
+                target="_blank"
+              >
+                Life Choice Academy</a
+              >
+              for allowing me the opportunity to persue my my dream! I have been
+              in the life Choice programme for about 4 months now and let my
+              tell you it is an experience of a life-time. Life Choices
+              introduced me to a whole new world, a world filled opportunities
+              to make myself great!
+            </p>
+          </div>
+          <div class="col d-flex justify-content-center my-5 align-items-end">
+            <img
+              id="antonio"
+              src="https://i.postimg.cc/XqFnBDX2/IMG-1478-1.jpg"
+            />
+          </div>
+        </div>
+        <hr class="w-50 mx-auto my-5 break" />
+      </div>
+    </div>
+    <div class="mb-5 mt-5 my-5">
+      <h1 class="skill mt-5">MY SKILL'S</h1>
+      <hr class="underline" />
+    </div>
+    <div class="flex-container p-4 w-100">
+      <h2 class="mx-auto d-flex justify-content-center my-5 offer capable">
+        WHAT I BRING TO THE TABLE...
+      </h2>
+    
+  
+  <div class="row mx-auto mt-5 text-center">
+    <div class="col-12 col-md-4 intro text-center">
+      <div>
+        <div>
+          <h2 class="lead mb-5">DESIGN</h2>
+        </div>
+          <div class="animated animate__bounceInLeft mb-5" style="animation-delay: 2s;">
+            <i class="fa-solid fa-pen-nib fa-beat-fade" style="color: #8b8e92;"></i>
+            </div>
+      </div>
+      <div>
+        <p class="styling my-5">
+          As a developer I love designing, my website look modern and I have a
+          creative mind.
+        </p>
+      </div>
+    </div>
+    <div class="col-12 col-md-4 intro text-center mb-5">
+      <div>
+        <div>
+          <h2 class="mb-5 lead">DEVELOPMENT</h2>
+        </div>
+        <div class="animated animate__fadeInDownBig" style="animation-delay: 2s;">
+          <i class="fa-solid fa-computer fa-beat-fade" style="color: #696c72;"></i>
+        </div>
+     
+      </div>
+      <div class="vl">
+        <p class="styling my-5">
+          I build scalable websites from scratch that fit seamlessly with
+          design. My focus is on micro animations, transitions and interaction.
+        </p>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-4 intro text-center">
+      <div>
+        <h2 class="mb-5 ps-4 lead">HOSTING</h2>
+      </div>
+      <div class="animated animate__bounceInRight" style="animation-delay: 2s;">
+        <i class="fa-solid fa-globe fa-beat-fade" style="color: #75777a;"></i>
+      </div>
+      <div class="vl">
+        <p class="styling my-5 ps-4">
+          I host websites on secure domains and have your site running fast and
+          efficiently.
+        </p>
+      </div>
+    </div>
+</div>
+</div>
+    <div class="container my-5 pt-5 px-5 mx-auto">
+      <div class="row mx-auto">
+      <div class="col-12 col-md-3 mb-4 mx-auto">
+<div class="card mx-auto" id="web">
+  <div class="percent">
+    <svg class="d-flex justify-content-center align-items-center">
+      <circle  cx="100" cy="75" r="60"></circle>
+      <circle cx="100" cy="75" r="60" class="first" style="--percent: 52"></circle>
+    </svg>
+    <div class="number">
+      <h3><span><i class="fa-brands fa-html5 fa-flip" style="color: #05d61e;"></i></span></h3>
+    </div>
+  </div>
+  <div class="title">
+    <h2 class="styling">html5</h2>
+  </div>
+</div></div>
+      <div class="col-12 col-md-3 mb-4">
+<div class="card mx-auto" id="css">
+  <div class="percent">
+    <svg>
+      <circle cx="100" cy="75" r="60"></circle>
+      <circle cx="100" cy="75" r="60" class="sec" style="--percent: 54"></circle>
+    </svg>
+    <div class="number">
+      <h3><span><i class="fa-brands fa-css3 fa-flip" style="color: #3adfb6;"></i></span></h3>
+    </div>
+  </div>
+  <div class="title">
+    <h2 class="styling">css</h2>
+  </div>
+</div></div>
+      <div class="col-12 col-md-3 mb-4">
+<div class="card mx-auto" id="boot">
+  <div class="percent">
+    <svg>
+      <circle cx="100" cy="75" r="60"></circle>
+      <circle cx="100" cy="75" r="60" class="third" style="--percent: 42"></circle>
+    </svg>
+    <div class="number">
+      <h3><span><i class="fa-brands fa-bootstrap fa-flip" style="color: #8843a8;"></i></span></h3>
+    </div>
+  </div>
+  <div class="title">
+    <h2 class="styling">bootstrap</h2>
+  </div>
+</div></div>
+      <div class="col-12 col-md-3 mb-4">
+<div class="card mx-auto" id="java">
+  <div class="percent">
+    <svg>
+      <circle cx="100" cy="75" r="60"></circle>
+      <circle cx="100" cy="75" r="60" class="fourth" style="--percent: 39"></circle>
+    </svg>
+    <div class="number">
+      <h3><span><i class="fa-brands fa-square-js fa-flip" style="color: #fe0101;"></i></span></h3>
+    </div>
+  </div>
+  <div class="title">
+    <h2 class="styling">javascript</h2>
+  </div>
+</div></div>
+    </div>
+    </div>
+</div>
+  
+
+
+</template>
+<script>
+import { Icon } from "@iconify/vue";
+
+export default {
+  components: {
+    Icon,
+  },
+};
+</script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Kalam:wght@300&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poltawski+Nowy:ital@1&display=swap");
+.quote {
+  font-family: "Kalam", cursive;
+  font-weight: bolder;
+  font-style: italic;
+  color: #646464 !important;
+}
+
+.quotes {
+  box-shadow: rgba(0, 0, 0, 1) 0px 5px 20px 5px;
+}
+
+.styling,
+.capable {
+  font-size: 25px;
+  font-family: 'Poltawski Nowy';
+  font-style: italic;
+}
+
+.learn{
+  font-size: 40px;
+  color: #3498db;
+  
+}
+.intro,
+.skill {
+  font-size: 35px;
+}
+.skill {
+  font-size: 50px;
+   font-family: 'Poltawski Nowy';
+  font-style: italic;
+}
+.lead{
+   font-family: 'Poltawski Nowy';
+  font-style: italic;
+  color: #3498db;
+  font-size: 25px;
+}
+
+.icon {
+  font-size: 1.7rem;
+}
+#see{
+  background-color:rgb(76, 127, 139) !important;
+}
+
+.flex-container{
+  background-color: rgb(214, 214, 214) !important;
+  /* min-width: 100% !important; */
+  border-radius: 20px;
+}
+
+img {
+  height: 600px;
+  width: 400px;
+  border-radius: 30px;
+}
+
+a {
+  color: #646464;
+}
+#see {
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.8) 0px 5px 20px 5px;
+  border-radius: 5px;
+}
+
+.break {
+  border-top: 5px black solid;
+}
+.underline {
+  border-top: 3px black solid;
+  width: 23%;
+}
+
+.vl {
+  border-left: 2px solid #646464;
+  height: 200px;
+}
+
+.bootstrap {
+  background-color: rgb(88, 53, 120) !important;
+}
+
+.main{
+  margin-top:6rem !important ;
+}
+#web{
+  margin-right:rem !important ;
+}
+
+.card {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-color: #ffffff;
+  margin: 0 20px;
+  width: 200px;
+  height: 300px;
+  border-radius: 5px;
+  box-shadow: 0 10px 20px -10px rgba(0, 0, 0, 0.2);
+}
+
+.card .percent {
+  position: relative;
+}
+
+.card svg {
+  width: 100%;
+  height: 100%;
+  transform: rotate(-90deg);
+}
+
+.card svg circle {
+  /* width: 200px !important;
+  height: 300px !important; */
+  margin-right: 50px !important;
+  position: absolute;
+  fill: none;
+  stroke: #f0f0f0;
+  stroke-width: 10;
+  stroke-linecap: round;
+}
+
+.card svg circle:last-of-type {
+  stroke-dasharray: 625px;
+  stroke-dashoffset: calc(625px - (625px * var(--percent)) / 100);
+  stroke: #3498db; 
+}
+
+.card .number {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.card .number h3 {
+  font-weight: 200;
+  font-size: 3.5rem;
+}
+
+.card .number h3 span {
+  font-size: 2rem;
+}
+
+.card .title h2 {
+  margin: 25px 0 0;
+}
+.first{
+  stroke: #2ecc71 !important;
+}
+#web:hover{
+  box-shadow:#2ecc71  0px 5px 5px 5px;
+  scale:102% ;
+}
+#css:hover{
+  box-shadow:skyblue  0px 5px 5px 5px;
+  scale:102% ;
+}
+#boot:hover{
+  box-shadow:rgb(218, 48, 218)  0px 5px 5px 5px;
+  scale:102% ;
+}
+#java:hover{
+  box-shadow:rgb(240, 124, 81)  0px 5px 5px 5px;
+  scale:102% ;
+}
+
+ 
+
+.sec{
+  stroke: skyblue !important;
+}
+.third{
+  stroke: rgb(182, 1, 182) !important;
+}
+.fourth{
+  stroke: orangered !important;
+}
+
+@media screen  and (max-width:300px) {
+  img{
+    height: 300px;
+    width: 250px;
+  }
+  .quote{
+    font-size: 25px;
+  }
+  
+  .styling{
+  font-size:14px ;  
+  }
+  .intro{
+    margin-top: 10px;
+    font-size: 20px;
+
+  
+}
+.skill{
+  font-size: 30px ;
+  margin-top: 40px !important;
+}
+.offer{
+  font-size: 25px ;
+}
+.lead{
+  font-size: 20px ;
+}
+.underline{
+  width:15% ;
+}
+.vl{
+  height:80px;
+}
+
+}
+</style>
